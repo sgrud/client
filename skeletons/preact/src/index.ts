f987de84a024f986658e73b6ad10747c9ed7e38f
Philip Schildkamp <phil@codeschluss.de>
2021-11-22 13:59:32 +0100

    Refine skeletons
    - Fix jsx compiler error (preact)
    - Replace custom `types.d.ts` with `@types/css-modules` package
    - make `@sgrud/bin` a necessary dependency (where `runtimify` is needed)

--- skeletons/preact/src/index.ts
+++ skeletons/preact/src/index.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./types.d.ts"/>
-
 import register from 'preact-custom-element';
 import PreactComponent from './component/PreactComponent';
 
--- skeletons/preact/src/types.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.sass' {
-  export default styles;
-  const styles: string;
-}
