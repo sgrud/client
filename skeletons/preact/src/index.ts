d86891e9c87f24d61c52e7ef3dba3106ff7ff1c7
Philip Schildkamp <phil@codeschluss.de>
2022-11-13 15:09:16 +0100

    Externalize skeletons and examples
    - Skeletons moved to https://github.com/sgrud/skeletons
    - Examples moved to https://github.com/sgrud/demos

--- skeletons/preact/src/index.ts
+++
@@ -1,6 +0,0 @@
-import register from 'preact-custom-element';
-import { PreactComponent } from './component/PreactComponent';
-
-register(PreactComponent, PreactComponent.tagName, undefined, {
-  shadow: true
-});
